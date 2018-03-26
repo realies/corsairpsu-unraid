9cd8cbc90ab42ee333ec6aa3fd2f1e54bc000310
realies <5107843+realies@users.noreply.github.com>
2018-03-27 01:28:58 +0300

    alpha release

---
+++ README.md
@@ -0,0 +1,9 @@
+## Introduction
+This plugin is a wrapper for [corsairmi](https://github.com/notaz/corsairmi) which reads out monitoring information for Corsair RMi and HXi power supplies.
+
+![Screenshot](https://i.snag.gy/Z0khmv.jpg)
+## Usage
+Plugins > Install Plugin
+```
+https://raw.githubusercontent.com/realies/corsairpsu-unraid/master/corsairpsu.plg
+```
---
+++ src/corsairpsu/usr/local/emhttp/plugins/corsairpsu/status.php
@@ -0,0 +1,19 @@
+<?php
+$stdout = shell_exec("/usr/local/bin/corsairmi 2>/dev/null");
+$re = "/(?<key>[^:]+):\s+\'*(?<value>[^\n\']+)\'*\s*/";
+preg_match_all($re, $stdout, $matches, PREG_SET_ORDER, 0);
+foreach($matches as $match)
+ $data[$match["key"]] = $match["value"];
+if(!isset($data))
+ exit(json_encode(array($stdout)));
+$capacity = filter_var($data["product"], FILTER_SANITIZE_NUMBER_INT);
+$load = round($data["total watts"] / $capacity * 100);
+echo json_encode(array($data["uptime"],
+"{$data["temp1"]} / {$data["temp2"]}",
+$data["fan rpm"],
+$data["supply volts"],
+$capacity,
+"{$data["output0 watts"]} Watts",
+"{$data["total watts"]} Watts",
+$load));
+?>
